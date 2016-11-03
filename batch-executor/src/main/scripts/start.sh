103622d8b97e58a1f3f77ed77d59b90642837ade
liyk@shicaidai.com <liyk@shicaidai.com>
2016-11-03 21:58:33 +0800

    interest

--- batch-executor/src/main/scripts/start.sh
+++ batch-executor/src/main/scripts/start.sh
@@ -19,7 +19,7 @@ CLASSPATH=$CLASSPATH:$CONFIG_ROOT:$LIB_ROOT/*
 
 JAVA_OPTS="-Xms4096m -Xmx4096m -XX:NewSize=512m -server -XX:+DisableExplicitGC -Dlog.path=$LOG_PATH -verbose:gc -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:$LOG_PATH/logs/gc.log"
 
-java -classpath $CLASSPATH $JAVA_OPTS com.baidu.fbu.fcore.common.job.BatchLauncher -f $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -classpath $CLASSPATH $JAVA_OPTS com.scd.batch.executor.job.BatchLauncher -p classpath:META-INF/ApplicationContext-executor.xml -f $1 $2 $3 $4 $5 $6 $7 $8 $9
 
 RETVAL=$?
 
