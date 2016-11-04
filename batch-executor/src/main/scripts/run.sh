ea717c1ac8345084aba35bc75d6b4daca937a7b6
liyk@shicaidai.com <liyk@shicaidai.com>
2016-11-04 15:55:45 +0800

    run.sh

---
+++ batch-executor/src/main/scripts/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone http://172.18.0.15/git/batch.git
+mvn clean compile package install
+cd batch-executor/target
+unzip BatchLauncher-bin.zip
+cd BatchLauncher
+chmod +x bin/start.sh
+bin/start.sh -n switch
+bin/start.sh -n prepare
