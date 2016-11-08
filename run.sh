7a2f92dc8fb18bb5e61b870ce3a7bf93b16893dd
liyk@shicaidai.com <liyk@shicaidai.com>
2016-11-08 15:27:43 +0800

    ignore

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-git clone http://172.18.0.15/git/batch.git
+#git clone http://172.18.0.15/git/batch.git
+git pull origin master
 mvn clean compile package install
 cd batch-executor/target
 unzip BatchLauncher-bin.zip
