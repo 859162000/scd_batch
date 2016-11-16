a83aead898148ba2bf916077e1c41d4b723d022e
liyk@shicaidai.com <liyk@shicaidai.com>
2016-11-16 18:38:58 +0800

    script

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
+
 #git clone http://172.18.0.15/git/batch.git
 git pull origin master
 mvn clean compile package install
-cd batch-executor/target
-unzip BatchLauncher-bin.zip
-cd BatchLauncher
-chmod +x bin/start.sh
+[ -d BatchLauncher ] && rm -rf BatchLauncher
+unzip batch-executor/target/BatchLauncher-bin.zip
+chmod +x BatchLauncher/bin/*
